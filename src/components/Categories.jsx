f4f506c7a8045d971eacfd7e817d5d46286e5e23
Soham2402 <sohamhegdessj4@gmail.com>
2023-12-17 04:07:31 +0530

    error handling

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -73,7 +73,7 @@ const Categories = () => {
         <Link to={`/categories/store/${items[2].id}`} className='relative bottom-32 hover:border-pink-500 border-button rounded-lg hover:scale-105 transition-transform cursor-pointer border-2 m-auto bg-button p-2 w-[60vw] h-[60vw] text-center md:w-[25vw] md:h-[25vw] lg:w-[20vw] lg:h-[20vw] shadow-tight '> 
           <div className=' flex flex-col items-center justify-center h-[100%]'>
           <div className=' text-7xl font-bold uppercase text-pink-500'>{hasWhiteSpace(items[2].name)}</div>
-            <div className=' text-md shadow-ti uppercase text-pink-500 font-nica font-bold'>{items[3].name}</div>
+            <div className=' text-md shadow-ti uppercase text-pink-500 font-nica font-bold'>{items[2].name}</div>
           </div>
         </Link>
             </div>
